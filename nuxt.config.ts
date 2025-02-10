eeab6855d4f00fff5c6cfbcc54622de4cb863304
Dewsara-Insighture <dewsara.wijerathne@insighture.com>
2025-02-10 15:44:13 +0530

    sample added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,6 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
   modules: ['@vite-pwa/nuxt'],
-  app: {
-    baseURL: '/user/'  // This ensures the app serves under /user/
-  },
   pwa: {
     registerType: 'autoUpdate',
     manifest: {
